52f8381036a83ff3c4a13beca5b74713d35e6b6d
aguamole <56515790+aguamole@users.noreply.github.com>
2024-12-27 00:32:27 -0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,15 +2,15 @@
 
 CAMINHO="$HOME/.scripts/hypr/"
 # Caminho do arquivo config.py e do ambiente virtual
-config_path="${CAMINHO}/config.py"
+CONFIG_PATH="${CAMINHO}/config.py"
 
 # Verifica se o processo já está em execução
-if pgrep -f "$config_path" > /dev/null; then
+if pgrep -f "${CONFIG_PATH}" > /dev/null; then
     # Se o processo estiver em execução, finaliza o processo
     echo "O processo já está em execução. Finalizando..."
-    pkill -f "$config_path"
+    pkill -f "${CONFIG_PATH}"
 else
     # Se o processo não estiver em execução, inicia o processo
     echo "O processo não está em execução. Iniciando..."
-    $config_path   # O "&" não faz o menor sentido aqui, ele é desnecessario
+    ${CONFIG_PATH}   # O "&" não faz o menor sentido aqui, ele é desnecessario
 fi
